54e70be3f6f64f8c05222504adf9ec0c9d65cbf0
Christian Herskind Sløk <cherskindslok@gmail.com>
2026-01-13 13:24:10 +0100

    Add initial project files for AddSeparator application with .NET 9.0 framework

---
+++ AddSeparator/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ToThePowerOf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Test the method with the given examples
+            Console.WriteLine(AddSeparator("ABCD"));
+            Console.WriteLine(AddSeparator("chocolate"));
+        }
+
+        static string AddSeparator(string input)
+        {
+            
+        }
+    }
+}
