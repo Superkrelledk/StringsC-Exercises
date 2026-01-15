7a742dafa542d1deb7acf2e1b27dd871fe53d001
Christian Herskind Sløk <cherskindslok@gmail.com>
2026-01-15 08:48:47 +0100

    Add initial project files and configurations for NumberOFWords application
    - Created NumberOFWords.dll and NumberOFWords.dll reference files for .NET 9.0.
    - Added project.assets.json to manage NuGet package dependencies.
    - Included project-specific NuGet configuration files: NumberOFWords.csproj.nuget.dgspec.json, NumberOFWords.csproj.nuget.g.props, and NumberOFWords.csproj.nuget.g.targets.
    - Established project structure for restoring and managing packages.

---
+++ NumberOFWords/NumberOFWords/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace NumbersOfWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NumbersOfWords("This is Samle sentence"));
+            
+            Console.WriteLine(NumbersOfWords("Ok"));
+            Console.ReadLine();
+        }
+
+
+        static int NumbersOfWords(string input)
+        {
+            int length = 0;
+            foreach (char c in input)
+            {
+                // Mellemrum tæller som et ord
+                if (c == ' ')
+                {
+                    // Øg længden med 1 for hvert mellemrum
+                    length++;
+                }
+            }
+            // Tilføj 1 for det sidste ord, der ikke følges af et mellemrum
+            return length + 1;
+        }
+    }
+}
